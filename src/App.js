31c8138c2dfdcc82c2ae6e5fc76373051fa97b96
ThuKhaCharmThar <thukhachanthar.dev@example.com>
2024-02-27 17:05:14 +0630

    Completed Birds

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Home from "./Home";
+import Navbar from "./Navbar";
+import BirdDetails from "./BirdDetails";
+import NotFound from "./NotFound";
+import BirdList from "./BirdList";
+import Birds from "./Birds";
+
+function App() {
+  return (
+    <Router>
+      <div className="App">
+        <Navbar />
+        <div className="content">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/BirdList" element={<Birds />} />
+            <Route path="/birds/:id" element={<BirdDetails />} />
+            <Route path="*" element={<NotFound />} />
+          </Routes>
+        </div>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
